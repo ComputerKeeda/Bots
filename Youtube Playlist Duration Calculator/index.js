819fb7f86fd0ec3cf02fd95cf776f272c9521284
Shubham Sharma <72196623+ComputerKeeda@users.noreply.github.com>
2022-12-30 13:34:48 +0530

    Update index.js

--- Youtube Playlist Duration Calculator/index.js
+++ Youtube Playlist Duration Calculator/index.js
@@ -2,20 +2,32 @@ let a = document.querySelectorAll(
   "span#text.style-scope.ytd-thumbnail-overlay-time-status-renderer"
 );
 let playlist = [];
+let playlistInCorrectOrder = [];
 let time = 0;
 for (let i = 0; i < a.length; i++) {
-  const ref = a[i].innerHTML;
-  playlist[i] = ref.trim().split(":");
+  const span = a[i].innerHTML;
+  playlist[i] = span.trim().split(":");
 }
-playlist.forEach((e) => {
-  i = 1;
-  e.reverse().forEach((t) => {
-    time += i * parseInt(t);
-    i = i * 60;
+
+for (let index = 0; index < playlist.length; index++) {
+  const element = playlist[index];
+  let tempArray = element.reverse();
+  let numberValues = tempArray.map(function (str) {
+    return parseInt(str);
   });
-});
-let hr = Math.round(time / 3600);
-let min = Math.round((time % 3600) / 60);
+  playlistInCorrectOrder[index] = numberValues;
+}
+
+for (let index = 0; index < playlistInCorrectOrder.length; index++) {
+    const duration = playlistInCorrectOrder[index];
+    let timeUnit = 1
+    for (let i = 0; i < duration.length; i++) {
+        time += duration[i] * timeUnit;
+        timeUnit *= 60;
+    }
+}
+let hr = parseInt(time / 3600);
+let min = parseInt((time % 3600) / 60);
 let sec = time % 60;
 
 const playlist_data = {
